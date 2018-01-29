893e75256a6e9224eaa971b5465bf5b0b98ec468
zhangzb <zhangzb@sci99.com>
2018-01-29 11:54:06 +0800

    **init

---
+++ GradualSegmentControl/GradualSegmentControl/GradualSegmentControl/ZBGradualSegmentControl.swift
@@ -0,0 +1,21 @@
+//
+//  ZBGradualSegmentControl.swift
+//  GradualSegmentControl
+//
+//  Created by 张志彬 on 2018/1/29.
+//  Copyright © 2018年 张志彬. All rights reserved.
+//
+
+import UIKit
+
+class ZBGradualSegmentControl: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
